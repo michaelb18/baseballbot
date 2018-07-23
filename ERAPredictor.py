127147e28e6610180de949829cccf6871a49d513
ranger6777 <ranger6777@gmail.com>
2018-07-23 05:20:12 -0700

    Added some comments

--- ERAPredictor.py
+++ ERAPredictor.py
@@ -1,3 +1,4 @@
+#1. import libraries
 from __future__ import print_function
 
 import glob
@@ -18,14 +19,16 @@ tf.logging.set_verbosity(tf.logging.ERROR)
 pd.options.display.max_rows = 10
 pd.options.display.float_format = '{:.1f}'.format
 
+#2. read in dataset
 baseball=pd.read_csv("../../baseballdatabank/core/Pitching.csv",sep=',')
+baseball=baseball.dropna(axis=0, how="any")
 
+#3. split features and labels
 def preproccessDataset(dataset):
 	feature_dict={};
-	cat=dataset['ERA']
-	names=dataset['playerID']
-	#cat=cat.tail(2000)
-	#names=names.tail(2000)
+	cat=np.asarray(dataset['ERA'])
+	names=np.asarray(dataset['playerID'])
+
 	count=0;
 	for i in cat:
 		
@@ -50,6 +53,10 @@ def preproccessDataset(dataset):
 
 features,labels=preproccessDataset(baseball)
 print(len(labels))
+
+#4. split training and testing (doesn't happen in this program due to a lack of data).
+#5. split data into batches (doesn't happen in this program due to a lack of data).
+#note: not used, using full gradient decenst due to a lack of data
 def input_fn(batch_size,features,labels,numEpochs):
 	#features={key:np.array(value) for key,value in dict(features).items()}
 	ds = Dataset.from_tensor_slices((features,labels))
@@ -57,22 +64,42 @@ def input_fn(batch_size,features,labels,numEpochs):
 	features,labels=ds.make_one_shot_iterator().get_next()
 	return features,labels
 
-
+#6. train the model
 inputs=tf.placeholder(dtype=tf.float32,shape=(None,3))
 ls=tf.placeholder(dtype=tf.float32)
+step=tf.Variable(dtype=tf.int32, initial_value=0)
 print(inputs.shape)
-dense1=tf.layers.dense(inputs=inputs,units=70,activation=tf.nn.relu)
-outputs=tf.layers.dense(inputs=dense1,units=70,activation=tf.nn.relu)
-loss=tf.losses.absolute_difference(ls,outputs)
-opt=tf.train.GradientDescentOptimizer(learning_rate=.001,name="opt").minimize(loss)
-#opt=tf.contrib.estimator.clip_gradients_by_norm(opt,5.0)
+
+#make the neural network's structure (using low level tf api)
+dense1=tf.layers.dense(inputs=inputs,units=35,activation=tf.nn.relu,name='dense1')
+dense2=tf.layers.dense(inputs=dense1,units=35,activation=tf.nn.relu,name='dense2')
+output_layer=tf.layers.dense(inputs=dense1,units=1,activation=tf.nn.relu,name='output_layer')
+step=tf.Variable(0,trainable=False,name="step")
+num_hidden_per_layer=35
+num_out=1
+num_in=3
+num_epochs=130
+
+#make loss function and gradient descent
+loss=tf.losses.absolute_difference(ls,output_layer)
+opt=tf.train.GradientDescentOptimizer(learning_rate=.001,name="opt").minimize(loss,global_step=step)
+
 init=tf.global_variables_initializer()
+save=tf.train.Saver(max_to_keep=7)
 with tf.Session() as sess:
 	sess.run(init)
-	for i in range(2000):
-		feature,label=input_fn(batch_size=5,features=features,labels=labels, numEpochs=1)
-		#print("Feature:",feature.eval())
-		sess.run(opt,feed_dict={inputs:feature.eval(),ls:label.eval()})
-		print(sess.run(loss,feed_dict={inputs:feature.eval(),ls:label.eval()}))
-
+	save=tf.train.import_meta_graph('models/model.ckpt-1000.meta')
+	save.restore(sess,tf.train.latest_checkpoint("./models"))
 	
+	for i in range(20000):
+		#feature,label=input_fn(batch_size=150,features=features,labels=labels, numEpochs=1)
+		sess.run(opt,feed_dict={inputs:features,ls:labels})
+		step=tf.add(step,1)
+		if(i%500==0):
+			print("Saving model at step:",i,". Loss:",sess.run(loss,feed_dict={inputs:features,ls:labels}))
+			save.save(sess,"models/model.ckpt",global_step=step.eval(),write_meta_graph=False)
+		
+	save.save(sess,"models/model.ckpt",global_step=step.eval(),write_meta_graph=False)#,write_meta_graph=False
+
+	print("Model Saved. Save Path: model.ckpt")
+#7. train the model
