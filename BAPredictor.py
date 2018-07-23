6a4d3b301cff54e13e890c5fbe7286ff59316dfb
ranger6777 <ranger6777@gmail.com>
2018-07-23 05:18:22 -0700

    added some comments

--- BAPredictor.py
+++ BAPredictor.py
@@ -1,10 +1,12 @@
+#1. import libraries
 import numpy as numpy
 import csv
 import tensorflow as tf
 #BA predictor
 
+#2. read in data
 input_dict={}
-with open('../baseballdatabank/core/Batting.csv','r') as csvfile:
+with open('../../baseballdatabank/core/Batting.csv','r') as csvfile:
 
 	readCSV=csv.reader(csvfile,delimiter=',')
 	for row in readCSV:
@@ -15,7 +17,8 @@ with open('../baseballdatabank/core/Batting.csv','r') as csvfile:
 						input_dict.get(row[0]).append((int(row[8]))/float(int(row[6])))
 				else:
 					if(float(int(row[6]))!=0):				
-						input_dict[row[0]]=[int(row[8])/float(int(row[6]))]			
+						input_dict[row[0]]=[int(row[8])/float(int(row[6]))]	
+#3. split features and labels		
 inputList=list()
 outputList=list()
 for k in input_dict:
@@ -29,8 +32,10 @@ for k in input_dict:
 		l.append(input_dict.get(k)[size-2])
 		inputList.append(l)		
 
+#4. split training and testing (doesn't happen in this program due to a lack of data)
+#5. split data into batches (doesn't happen in this program due to a lack of data) 
 
-#hyperparams
+#make the neural network (using lowest level tensorflow api)
 num_hidden_per_layer=25
 num_out=1
 num_in=3
@@ -38,22 +43,29 @@ num_epochs=130
 input_label=tf.placeholder(tf.float32)
 output_label=tf.placeholder(tf.float32)
 
+#make weights 
 first_weight=tf.Variable(tf.random_uniform([num_in,num_hidden_per_layer],-1,1))
 second_weight=tf.Variable(tf.random_uniform([num_hidden_per_layer,num_hidden_per_layer],-1,1))
 third_weight=tf.Variable(tf.random_uniform([num_hidden_per_layer,num_hidden_per_layer],-1,1))
 fourth_weight=tf.Variable(tf.random_uniform([num_hidden_per_layer,num_out],-1,1))
 
+#make layers
+#each layer is a mathematical operation. In each line, I multiply weights by inputs and then plug that into an activation function. In math terms, I do y=activation(mx). We'll worry about what an activation function is and why its used later, the important part is that each layer does y=mx+b (m=weights, x=features, b=bias , bias is 0).
+
+#used 4 layers because that's what I found worked best through experimentation
 first_hidden_layer=tf.sigmoid(tf.matmul(input_label,first_weight))
 second_hidden_layer=tf.sinh(tf.matmul(first_hidden_layer,second_weight))
 third_hidden_layer=tf.sigmoid(tf.matmul(second_hidden_layer,third_weight))
 dropout=tf.layers.dropout(inputs=third_hidden_layer)
 output_layer=tf.sigmoid(tf.matmul(dropout,fourth_weight))
+#note - one layers output is the next layers input.
 
+#make the loss function
 loss=tf.reduce_mean(output_layer-output_label)
 opt=tf.train.GradientDescentOptimizer(.001).minimize(loss)
 
 init=tf.initialize_all_variables()
-
+#6. train the model
 with tf.Session() as sess:
 	sess.run(init)
 	err=sess.run(loss, feed_dict={input_label:inputList, output_label:outputList})
@@ -62,11 +74,11 @@ with tf.Session() as sess:
 		sess.run(opt, feed_dict={input_label:inputList, output_label:outputList})
 		err=sess.run(loss, feed_dict={input_label:inputList, output_label:outputList})
 		i=i+1
-		if(i%100==0):
+		if(i%10==0):
 			print("Err:",err)
 			print("I:",i) 
-	print("Test Avg:",sess.run(output_layer,feed_dict={input_label:[[.302,.287,.288]]}))
+	print("Test Avg:",sess.run(output_layer,feed_dict={input_label:[[.314,.326,.320]]}))
 	print("Total Loss:",err)
 print("Done")
 
-
+#7. evalutate the model
